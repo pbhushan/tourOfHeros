d69d96efbae52fb990f6eaa9a5e9091122e0d0ab
Bhushan <PBHUSHAN@ets.org>
2016-08-01 22:04:04 -0400

    testing git push

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'my-app',
-    template : '<h1> Here comes Tour of Heros <h1>' 
+    template : '<h1> Here comes Tour of Heros <h1><h2> Checking Github</h2>' 
 })
 
 export class AppComponent{}
